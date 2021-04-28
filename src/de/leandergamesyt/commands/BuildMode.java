5c62ed3f94009bc28bf36ca4b78bdf69eac49530
xLixon <yekta.senay@kappagames.de>
2021-04-28 10:15:56 +0200

    safety

--- src/de/leandergamesyt/commands/BuildMode.java
+++ src/de/leandergamesyt/commands/BuildMode.java
@@ -16,27 +16,20 @@ public class BuildMode implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        System.out.println("1");
 
         if (!(sender instanceof Player)) {
-            System.out.println("2");
             sender.sendMessage("§7[§e!§7]§c Du musst ein Spieler sein, um in den BuildMode zu gehen!");
         } else {
-            System.out.println("3");
 
             Player p = (Player) sender;
 
             if (!(p.hasPermission("build.change"))) {
-                System.out.println("4");
                 p.sendMessage(LobbyPlus.noPermsError);
             } else {
-                System.out.println("5");
                 if (buildModePlayerList.contains(p.getName())) {
-                    System.out.println("6");
                     buildModePlayerList.remove(p.getName());
                     p.sendMessage("§7[§e!§7]§c Du bist nicht mehr im BuildMode!");
                 } else {
-                    System.out.println("7");
                     buildModePlayerList.add(p.getName());
                     p.sendMessage("§7[§e!§7]§c Du bist jetzt im BuildMode!");
                 }
--- src/de/osternachten/lobbyplus/osternachten/listener/listener/chat/ChatColorRed.java
+++ src/de/osternachten/lobbyplus/osternachten/listener/listener/chat/ChatColorRed.java
@@ -1,6 +1,3 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
 
 package de.osternachten.lobbyplus.osternachten.listener.listener.chat;
 
